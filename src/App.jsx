75eb1ae61e8bd3f4f77432463db41e786f322983
Aruukee <nurbekvvnass@example.com>
2026-04-22 23:24:48 +0600

    added branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import JobList from './components/JobList'
+import JobCard from './components/JobCard'
 
 const App = () => {
   return (
-    <div>
-      App
+    <div style={{ backgroundColor: "#f9f9f9", minHeight: "100vh" }}>
+      <JobList />
+      <JobCard />
     </div>
   )
 }
--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -1,12 +1,58 @@
-import React from 'react'
+import React, { useState }from 'react'
 import style from "../styles/JobList.module.css"
+import JobCard from './JobCard'
 
 const JobList = () => {
+  const [jobs, setJobs] = useState([
+    {
+      id: 1,
+      title: "Frontend Developer",
+      company: "Tech AI",
+      createdAt: "2026-04-20T10:00:00Z"},
+      {
+        id: 2,
+        title: "UI/UX Designer",
+        company: "Creative Studio",
+        createdAt: "2026-04-22T15:00:00Z"},
+        {
+          id: 3,
+          title: "Backend Node.js",
+          company: "Global Soft",
+          createdAt: "2026-04-21T09:00:00Z"},
+      ]);
+
+      const sortedJobs = [...jobs].sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+      const handleDelete = (id) => {
+        if (window.confirm("Бул вакансияны өчүргүңүз келеби?")) {
+          setJobs(jobs.filter(job => job.id !== id));
+        }
+      }
+      const [loading, setLoading] = useState(true);
+      if (loading) {
+        return (
+          <div style={{ textAlign: "center", padding: "50px" }}>
+            <p style={{ fontSize: "18px", color: "#555" }}>Жүктөлүүдө...</p>
+          </div>
+        )}
+};
+
   return (
-    <div>
-      
+    <div style={{ 
+     maxWidth: "600px",
+     margin: "0 auto",
+     padding: "20px"}}>
+     <h2 style={{ marginBottom: "20px" }}>вакансиялар тизмеси</h2>
+     {sortedJobs.length > 0 ? (
+      sortedJobs.map(job => (
+        <JobCard key={job.id}
+         job={job} 
+         onDelete={handleDelete} />
+      ))
+     ) : (
+      <p style={{ textAlign: "center", color: "#777" }}>Вакансиялар табылган жок</p>
+     )};
+    
     </div>
   )
-}
-
+      
 export default JobList
